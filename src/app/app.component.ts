d959256f50c8ab00c0128f79e468b6d9378f13a2
fritz <fritzner.neptune1.ctr@navy.mil>
2024-07-26 13:54:28 -0700

    end of module 3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,11 +12,11 @@ import { RouterOutlet } from '@angular/router';
 export class AppComponent implements OnInit {
   title = 'DatingApp';
   http = inject(HttpClient);
-  forecasts: any;
+  users: any;
 
   ngOnInit(): void {
-    this.http.get('https://localhost:5001/weatherforecast').subscribe({
-      next: response => this.forecasts = response,
+    this.http.get('https://localhost:5001/api/users').subscribe({
+      next: response => this.users = response,
       error: error => console.log(error),
       complete: ()=> console.log('Request has completed')
     });
